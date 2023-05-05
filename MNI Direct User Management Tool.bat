a75dd0805ee39097c1a225be8d2f8fbf03865116
spartan129 <128940775+spartan129@users.noreply.github.com>
2023-05-05 08:23:45 -0400

    Updated BAT File to run properly

--- MNI Direct User Management Tool.bat
+++ MNI Direct User Management Tool.bat
@@ -1,3 +1,3 @@
 @echo off
-powershell.exe -Command "Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0MNI_UM_Tool.ps1""' -Verb RunAs"
+powershell.exe -Command "Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0MNIDirect_UM_Tool.ps1""' -Verb RunAs"
 
